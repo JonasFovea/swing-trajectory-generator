26307095770da20c52c982a531fdcf5fef3b4aa6
Jonas Grube <jonas.grube@fh-bielefeld.de>
2022-06-19 21:29:03 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='swinggen',
-    version='0.1.1',
+    version='0.1.2',
     description='',
     py_modules=['swinggen'],
     package_dir={'': 'src'},
